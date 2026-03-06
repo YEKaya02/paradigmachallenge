ae32e25286987504e3bb8212d542e76c84888297
Yunus Kaya <yunusemrekaya@protonmail.com>
2026-03-06 15:47:38 +0100

    Pattern matching aan file read toegevoegd. Eerste iteratie van hoofdstuk 2 verslag gemaakt.

--- RLE.exs
+++ RLE.exs
@@ -1,2 +1,8 @@
-contents = File.read!("loremipsum.txt")
-IO.puts(contents)
+# Hier wordt aan de hand van pattern matching een bestand ingelezen.
+case File.read("loremisum.txt") do
+  {:ok, contents} ->
+    IO.puts(contents)
+
+  {:error, reason} ->
+    IO.puts(reason)
+end
