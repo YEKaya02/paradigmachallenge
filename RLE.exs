f5ebdacdb51b73752a886eb9dfba69c028c738ca
Yunus Kaya <yunusemrekaya@protonmail.com>
2026-03-08 15:39:53 +0100

    RLE naar RLECompress hernoemd. Errors toegevoegd die gedocumenteerd staan bij de File.read functie

--- RLE.exs
+++ RLE.exs
@@ -1,4 +1,4 @@
-defmodule RLE do
+defmodule RLECompress do
   # Start functie, hier wordt pattern matching gebruikt om te kijken of de input alleen een list is.
   def compress(list) do
     # Zet de initieele waarde, roept de functie recursief aan.
@@ -31,9 +31,24 @@ end
 # Hier wordt aan de hand van pattern matching een bestand ingelezen. De text uit het bestand wordt vervolgens in een array gezet.
 case File.read("loremipsum.txt") do
   {:ok, contents} ->
-    compressed_text = RLE.compress(String.graphemes(contents))
+    compressed_text = RLECompress.compress(String.graphemes(contents))
     IO.inspect(compressed_text)
 
+
+  {:error, :enoent} ->
+    IO.puts("File does not exits (enoent)")
+
+  {:error, :eacces} ->
+    IO.puts("Missing permission for reading the file, or for searching one of the parent directories (eacces)")
+
+  {:error, :eisdir} ->
+    IO.puts("The named file is a directory (eisdir)")
+
+  {:error, :enotdir} ->
+    IO.puts("A component of the file name is not a directory (enotdir)")
+
+  {:error, :enomem} ->
+    IO.puts("There is not enough memory for the contents of the file (enomem)")
 
   {:error, error} ->
     IO.puts(error)
